3b70ccd5e0748cb08589f6edb65f5833edcdf300
Will Temple <witemple@microsoft.com>
2020-09-11 20:33:16 -0700

    [dev-tool] changes to core rollup configuration

--- sdk/core/core-http/rollup.config.js
+++ sdk/core/core-http/rollup.config.js
@@ -1,5 +1,23 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import { makeConfig } from "@azure/dev-tool/shared-config/rollup";
+
+export default makeConfig(require("./package.json"));
+
+/*
 import * as base from "./rollup.base.config";
 
-const inputs = [base.nodeConfig(), base.browserConfig()];
+const inputs = [];
+
+if (!process.env.ONLY_BROWSER) {
+  inputs.push(base.nodeConfig());
+}
+
+// Disable this until we are ready to run rollup for the browser.
+if (!process.env.ONLY_NODE) {
+  inputs.push(base.browserConfig());
+}
 
 export default inputs;
+*/
